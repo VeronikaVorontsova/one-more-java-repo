c4e0b564ecfc03cc38ea490258117358b06a7997
Veronika <78588206+VeronikaVorontsova@users.noreply.github.com>
2023-10-10 21:20:52 +0300

    Create Hypot.java
    add Hypot

---
+++ Hypot.java
@@ -0,0 +1,15 @@
+public class Hypot {
+    public static void main(String args[]) {
+      double x;
+      double y;
+      double z; 
+      
+      x = 3;
+      y = 4; 
+      
+      z = Math.sqrt (x*x + y*y);
+      
+
+      System.out.println("Длина гипотенузы: " + z);
+    }
+}
